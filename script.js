b755cb4560ff5811d0db41ae2f0d2c63b942be28
Brayan1305 <larame.brayan@yahoo.com>
2026-06-24 15:01:32 +0200

    deuxieme page

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("bonjour")
+
+const inputs = document.querySelectorAll('input[type="checkbox"]');
+const elements = document.querySelectorAll('.item-evenement');
+const totalAffichage = document.getElementById('total-elements');
+
+function mettreAJourCompteur() {
+    let elementsVisibles = 0;
+    elements.forEach(el => {
+        if (window.getComputedStyle(el).display !== 'none') {
+            elementsVisibles++;
+        }
+    });
+    totalAffichage.textContent = elementsVisibles;
+}
+
+inputs.forEach(input => {
+    input.addEventListener('change', () => {
+        setTimeout(mettreAJourCompteur, 10);
+    });
+});
+
+window.addEventListener('DOMContentLoaded', mettreAJourCompteur);
